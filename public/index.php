226bc928dee8770632e25a8d2486cd4df1334117
Thabo <thabo.s.moyo@gmail.com>
2026-03-16 08:59:24 +0000

    add crud endpoints

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
+use App\Controllers\BookingController;
 use App\Controllers\Api\BookingController as ApiBookingController;
 
 require_once __DIR__ . '/../bootstrap.php';
@@ -11,6 +12,13 @@ $app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', [BookingController::class, 'index']);
+$app->get('/bookings/create', [BookingController::class, 'create']);
+$app->post('/bookings', [BookingController::class, 'store']);
+$app->get('/bookings/{id}/edit', [BookingController::class, 'edit']);
+$app->post('/bookings/{id}', [BookingController::class, 'update']);
+$app->post('/bookings/{id}/delete', [BookingController::class, 'destroy']);
 
 $app->get('/api/bookings', [ApiBookingController::class, 'index']);
 
