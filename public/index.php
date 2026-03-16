eb9ba3496d48efba20391b3e4cabf39ae7f2267d
Thabo <thabo.s.moyo@gmail.com>
2026-03-16 08:49:54 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
+<?php
 
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use App\Controllers\Api\BookingController as ApiBookingController;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/api/bookings', [ApiBookingController::class, 'index']);
+
+$app->run();
